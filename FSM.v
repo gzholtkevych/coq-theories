04b4cebb61b250716335cfd3ace4295564485944
gzholtkevych <g.zholtkevych@karazin.ua>
2021-03-20 09:25:05 +0200

    infiniteness of nat has proved

--- FSM.v
+++ FSM.v
@@ -2,23 +2,120 @@ Require Import Karazin.TypeProperties.
 Require Import List.
 Open Scope list_scope.
 
-Record FST : Type := mkFST {
-  input : Set;
-  output : Set;
-  state : Set;
-  do : state -> input -> state * output;
-  fin : Finite input /\ Finite output /\ Finite state
+
+Class FSM (X Z : Set)
+          (f : Z -> X -> Z) := {
+  input := X; state := Z; do := f;
+  fin_input : Finite input;
+  fin_state : Finite state;
+  run : state -> list input -> state := fix h s u :=
+    match u with
+      nil     => s
+    | a :: u' => do (h s u') a
+    end
 }.
 
-Fixpoint run (m : FST) (sini : state m)
-             (w : list (input m))
-         : (state m) * list (output m) :=
-  match w with
-    nil => (sini, nil)
-  | a :: w' => let (s, b) := do m sini a in
-               let (s', u') := run m s w' in
-               (s', b :: u')
+Inductive off_on : Set := off : off_on | on : off_on.
+Inductive set_reset : Set := set : set_reset | reset : set_reset.
+Definition switch (z : off_on) (x : set_reset) : off_on :=
+  match z, x with
+    off, reset => off
+  | off, set   => on
+  | on, reset  => off
+  | on, set    => on
   end.
+
+Instance switcher : FSM set_reset off_on switch.
+Proof.
+  split; exists (set :: reset :: nil) || exists (on :: off :: nil);
+  intro; destruct t; (left; reflexivity) || (right; left); reflexivity. 
+Defined.
+
+
+Class FSA {X Z : Set} {f : Z -> X -> Z} `{FSM X Z f}
+      (z0 : Z) (g : Z -> bool) := {
+  init := z0;
+  decision := g;
+  accept : list input -> bool := fun u => decision (run init u)
+}.
+
+Definition ok_on (s : off_on) : bool :=
+  match s with
+    off => false
+  | on => true
+  end.
+
+Definition ok_off (s : off_on) : bool :=
+  match s with
+    off => true
+  | on => false
+  end.
+
+Instance on_acceptor : FSA off ok_on.
+Proof. split. Defined.
+
+Instance off_acceptor : FSA off ok_off.
+Proof. split. Defined.
+
+Existing Instance on_acceptor.
+Example ex_1 :
+  accept (set :: reset :: set :: set :: nil) = true.
+Proof. now compute. Qed.
+
+Existing Instance off_acceptor.
+Example ex_2 :
+  accept (set :: reset :: set :: set :: nil) = false.
+Proof. now compute. Qed.
+
+Class FST (X Y Z : Set) (z0 : Z) (f : Z -> X -> Z)
+  (g : Z -> X -> Y) `{FSM X Z z0 f} : Type := {
+  output := Y; show := g;
+  fin_out : Finite output;
+  behaviour : list input -> list output :=
+    fix f u := match u with
+                 nil => nil
+               | a :: u' => (show (run init u') a) :: (f u')
+    end
+}.
+
+Section FSM_Examples.
+  Inductive off_on : Set := off : off_on | on : off_on.
+  Inductive set_reset : Set := set : set_reset | reset : set_reset.
+
+  Lemma fin_set_reset : Finite set_reset.
+  Proof.
+    exists (set :: reset :: nil). intro.
+    destruct t; simpl.
+    - now left.
+    - right. now left.
+  Qed.
+
+  Lemma fin_off_on : Finite off_on.
+  Proof.
+    exists (off :: on :: nil). intro.
+    destruct t; simpl.
+    - now left.
+    - right. now left.
+  Qed.
+
+  Definition f (z : off_on) (x : set_reset) : off_on :=
+    match z, x with
+      off, reset => off
+    | off, set   => on
+    | on, reset  => off
+    | on, set    => on
+    end.
+
+  Instance m : FSM set_reset off_on off f := {|
+    fin_input := fin_set_reset; fin_state := fin_off_on
+  |}.
+
+  Definition g (s : off_on) : bool :=
+    match s with off => false | on  => true end.
+
+  Instance ma : FSA set_reset off_on off f g := {}.
+  Proof. constructor.
+End FSM_Examples.
 
 Section FST_Example.
   Inductive A : Set := green : A | red : A.
--- TypeProperties.v
+++ TypeProperties.v
@@ -1,8 +1,8 @@
 Require Import List.
+Require Import Arith.Compare_dec.
+Require Import Arith.Le.
+Require Import Arith.Lt.
 Open Scope list_scope.
-Require Import Arith.PeanoNat.
-Open Scope nat_scope.
-
 
 Section Definitions.
   Variable T : Type.
@@ -11,17 +11,16 @@ Section Definitions.
   Definition Finite : Prop := exists e : list T, forall t : T, In t e.
 End Definitions.
 
-Example bool_is_Inhabited : Inhabited bool.
-Proof. now exists (true :: nil). Qed.
+Section Examples_for_Inhabited.
+  Example bool_is_Inhabited : Inhabited bool.
+  Proof. now exists (true :: nil). Qed.
 
-Example False_isnot_Inhabited : ~ Inhabited False.
-Proof.
-  intro. elim H. intros e H1. apply H1.
-  destruct e as [| t e']; [ trivial | now exfalso ].
-Qed.
-
-Example False_is_Finite : Finite False.
-Proof. now exists nil. Qed.
+  Example False_isnot_Inhabited : ~ Inhabited False.
+  Proof.
+    intro. elim H. intros e H1. apply H1.
+    destruct e as [| t e']; [ trivial | now exfalso ].
+  Qed.
+End Examples_for_Inhabited.
 
 Theorem not_Inhabited_is_Finite : forall T : Type, ~ Inhabited T -> Finite T.
 Proof.
@@ -30,14 +29,67 @@ Proof.
   exists (t :: nil). intro H'. discriminate H'.
 Qed.
 
-Example bool_is_Finite : Finite bool.
-Proof.
-  exists (true :: false :: nil). intro.
-  destruct t; [ now left | right; now left ].
-Qed.
+Section Examples_for_Finite.
 
-Theorem nat_isnot_Finite : ~ Finite nat.
-(*  The idea of the proof is to construct a function outside : list nat -> nat
-    meeting the requirement
+  Example False_is_Finite : Finite False.
+  Proof.
+    apply not_Inhabited_is_Finite.
+    exact False_isnot_Inhabited.
+  Qed.
+
+  Example bool_is_Finite : Finite bool.
+  Proof.
+    exists (true :: false :: nil). intro.
+    destruct t; [ now left | right; now left ].
+  Qed.
+
+  Section NatIsNotFinite.
+  (*  Here we prove that nat is not a finite type/
+      The idea of the proof is to construct a function
+      outside : list nat -> nat meeting the requirement
       forall e : list nat, ~ In (outside e) e.                                *)
-Admitted.
+
+    Fixpoint outside (ns : list nat) : nat :=
+      match ns with
+        nil     => 0
+      | n :: ns' => let n' := outside ns' in
+                    if le_lt_dec n' n then S n else n'
+      end.
+
+    Compute outside nil.
+    Compute outside (7 :: 3 :: 5 :: 2 :: nil).
+
+    Section Lemmas.
+      Variable e : list nat.
+
+      Lemma lt_member_outside : forall n : nat, In n e -> n < outside e.
+      Proof.
+        intros * H.
+        induction e as [| m e' IHe]; simpl.
+        - now exfalso.
+        - case H; intro HC.
+          + rewrite HC. case (le_lt_dec (outside e') n); intro HD;
+            [apply le_n | assumption].
+          + pose (IHe' := IHe HC).
+            case (le_lt_dec (outside e') m); intro HD.
+            * assert (HAux : n < m). now apply le_trans with (outside e').
+              now apply le_S.
+            * assumption.
+      Qed.
+
+      Lemma outside_list : ~ In (outside e) e.
+      Proof.
+        intro H.
+        pose (H1 := lt_member_outside (outside e) H).
+        now pose (H2 := lt_irrefl (outside e)).
+      Qed.
+    End Lemmas.
+
+    Theorem nat_isnot_Finite : ~ Finite nat.
+    Proof.
+      unfold Finite. intro. elim H. intros e H1.
+      pose (H2 := H1 (outside e)).
+      now pose (H2' := outside_list e).
+    Qed.
+  End NatIsNotFinite.
+End Examples_for_Finite.
